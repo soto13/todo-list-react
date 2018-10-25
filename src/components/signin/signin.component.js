603e5ea3d4a80fe57c96fb096ca5214e0b6ac6b5
Jose Soto <ezesoto13@gmail.com>
2018-10-25 11:36:59 -0600

    Added initial directories

---
+++ src/components/signin/signin.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SignInComponent extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hola mundo :)</h1>
+      </div>
+    )
+  }
+}
+
+export default SignInComponent;
